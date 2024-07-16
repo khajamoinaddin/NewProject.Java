cdcb2f140c4835fb031f6021c3d3cfafbb313171
khajamoinaddin <125530497+khajamoinaddin@users.noreply.github.com>
2024-07-16 13:42:32 +0530

    updated successfully

--- main.java
+++ main.java
@@ -1,12 +1,13 @@
  class A{
     public void start(){
-        System.out.println("Hai Khaja");
+        System.out.println("Hai Khaja Moinaddin");
     }
  }
- public class main {
+public class main {
         public static void main(String[] args){
             A obj=new A();
             obj.start();
 
         }
-    }
+} 
+
