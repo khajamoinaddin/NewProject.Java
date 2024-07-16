7691d52128afa0f4976b2865f20e90c75bf655a7
khajamoinaddin <125530497+khajamoinaddin@users.noreply.github.com>
2024-07-16 11:46:43 +0530

    Java project

---
+++ main.java
@@ -0,0 +1,12 @@
+ class A{
+    public void start(){
+        System.out.println("Hai Khaja");
+    }
+ }
+ public class main {
+        public static void main(String[] args){
+            A obj=new A();
+            obj.start();
+
+        }
+    }
